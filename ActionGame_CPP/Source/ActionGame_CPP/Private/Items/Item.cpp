1345f9b023e8842ae0968b77fcc878d671e9ba8d
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-07-30 02:55:15 +0900

    UnrealEngineのC++になれるためにいろいろな機能をテスト

---
+++ ActionGame_CPP/Source/ActionGame_CPP/Private/Items/Item.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//ÉAÉCÉeÉÄÇÃèàóùÇèëÇ≠
+
+#include "Items/Item.h"
+#include "ActionGame_CPP/DebugMacros.h"
+
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	float MovementRate = 50.f;
+	float RotationRate = 45.f;
+
+
+	AddActorWorldOffset(FVector(MovementRate * DeltaTime , 0.f    , 0.f));
+	AddActorWorldRotation(FRotator(0.f , RotationRate * DeltaTime , 0.f));
+
+	DRAW_SPHERE_SingleFrame(GetActorLocation());
+	DRAW_VECTOR_SingleFrame(GetActorLocation() , GetActorLocation() + GetActorForwardVector() * 100.f);
+
+}
