fbbcb729350b46e0a7dcada3074f41b8490064b2
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-07-31 00:27:17 +0900

    アセットの追加

--- ActionGame_CPP/Source/ActionGame_CPP/Private/Items/Item.cpp
+++ ActionGame_CPP/Source/ActionGame_CPP/Private/Items/Item.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+//テスト
 //アイテムの処理を書く
 
 #include "Items/Item.h"
@@ -9,25 +10,37 @@
 AItem::AItem()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
 }
 
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
+
+	int32 AvgInt =  Avg<int32>(1, 3);
+	UE_LOG(LogTemp , Warning , TEXT("Avg of 1 and 3 : %d") , AvgInt);
+
+	float AvgFloat = Avg<float>(3.45f, 7.86f);
+	UE_LOG(LogTemp, Warning, TEXT("Avg of 3.45 and 7.86 : %f"), AvgFloat)
 }
+
+float AItem::TransformSin()
+{
+
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	float MovementRate = 50.f;
-	float RotationRate = 45.f;
-
-
-	AddActorWorldOffset(FVector(MovementRate * DeltaTime , 0.f    , 0.f));
-	AddActorWorldRotation(FRotator(0.f , RotationRate * DeltaTime , 0.f));
-
-	DRAW_SPHERE_SingleFrame(GetActorLocation());
-	DRAW_VECTOR_SingleFrame(GetActorLocation() , GetActorLocation() + GetActorForwardVector() * 100.f);
-
+	RunningTime += DeltaTime;
 }
--- ActionGame_CPP/Source/ActionGame_CPP/Public/Items/Item.h
+++ ActionGame_CPP/Source/ActionGame_CPP/Public/Items/Item.h
@@ -15,8 +15,41 @@ public:
 	AItem();
 	virtual void Tick(float DeltaTime) override;
 
-
 protected:
 	virtual void BeginPlay() override;
 
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite , Category = "Sin Parametor")
+		float Amplitude = 0.25f;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite , Category = "Sin Parametor")
+		float TimeConstant = 5.f;
+
+
+	UFUNCTION(BlueprintPure)
+	float TransformSin();
+
+	UFUNCTION(BlueprintPure)
+	float TransformCos();
+
+
+	template<typename T>
+	T Avg(T First, T Second);
+
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		float RunningTime;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* ItemMesh;
+
+
 };
+
+template<typename T>
+inline T AItem::Avg(T First, T Second)
+{
+	return T(First + Second) / 2;
+}
