e8b92719e3f890b26b6d6a15a7532e8e2036dca3
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-07-31 04:02:01 +0900

    仮キャラクター移動が完成

--- ActionGame_CPP/Source/ActionGame_CPP/Private/Pawns/Bird.cpp
+++ ActionGame_CPP/Source/ActionGame_CPP/Private/Pawns/Bird.cpp
@@ -1,8 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Pawns/Bird.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Pawns/Bird.h"
 
 ABird::ABird()
 {
@@ -17,24 +20,53 @@ ABird::ABird()
 	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
 	BirdMesh->SetupAttachment(GetRootComponent());
 
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 300.f;//Target‚Ü‚Å‚Ì‹——£
 
+
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
+	ViewCamera->SetupAttachment(CameraBoom);
+
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 void ABird::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
+
+void ABird::MoveForward(float Value)
+{
+	if (Controller && (Value != 0.f))
+	{
+		FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, Value);
+	}
+}
+
+void ABird::MoveRight(float Value)
+{
+	if (Controller && (Value != 0.f))
+	{
+		FVector Right = GetActorRightVector();
+		AddMovementInput(Right, Value);
+	}
+}
+
 
 void ABird::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(FName("MoveForward"), this , &ABird::MoveForward);
+	PlayerInputComponent->BindAxis(FName("MoveRight")  , this,  &ABird::MoveRight  );
 
 }
 
